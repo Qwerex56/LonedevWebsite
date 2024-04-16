198dcff523c3bedde83379167dda9bc3984906ca
Adrian Czubaty <adriancz.prywatny@gmail.com>
2024-04-16 17:17:43 +0200

    Added styles for root.
    Other Chnages:
     - Added styles for project card

--- Components/obj/Debug/net8.0/Components.AssemblyInfo.cs
+++ Components/obj/Debug/net8.0/Components.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Components")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1862c1d36aa6b6b54c32de2f39df26705ef25dd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+def9ce571b5169bc3dc126dc6f7053b18a2ebd21")]
 [assembly: System.Reflection.AssemblyProductAttribute("Components")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Components")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
